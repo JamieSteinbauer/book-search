7596027a2d2cecf9965f97836801ccb0b5b6332f
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-07-25 20:47:41 -0400

    created me resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,16 @@
+const { UserInputError, AuthenticationError } = require("apollo-server-express");
+const { User } = require('../models');
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        me: async (_parent, _args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('__v -password')
+
+                return userData;
+            }
+            throw new AuthenticationError('Uh oh! Not logged in');
         }
     }
 };
